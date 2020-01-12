4ed315af669c8734361fc403856fbe8b0d0706fd
Giovani Aguirre <me@its-g.io>
2020-01-11 19:02:16 -0600

    Added # to chicken dinner ID selector. Also completely changes the infustructure of my javascript. Now successfully getting winners!

--- Part1/scripts.js
+++ Part1/scripts.js
@@ -1,20 +1,48 @@
-let player = document.getElementById("player");
-let winnerWinner = document.getElementById("chickenDinner");
+let playerID = document.getElementById("player");
+let turn = "X"
 
-function helper() {
-  let locked = []
+function play(num) {
+  nextMove(document.getElementById(num))
+}
 
-  return function(index) {
-    let element = document.getElementById(`${index}`);
+function switchTurns() {
+  return turn === "X" ? turn = "O" : turn = "X";
+}
 
-    if (locked.includes(index)) return;
-    player.innerText === "X" ?
-      (player.innerText = "O") && (element.innerHTML = "𝕏") :
-      (player.innerText = "X") && (element.innerHTML = "𝕆")
-    locked.push(index);
-
-    locked.length === 9 ? winnerWinner.innerHTML = "Draw!" : null;
+function nextMove(square) {
+  if (square.innerText === "") {
+    square.innerText = turn;
+    // if (winner(turn)) ;
+    switchTurns();
+    playerID.innerText = turn;
   }
 }
 
-let play = helper();
+function winner(player) {
+  let result = false;
+  if (checkRow(0, 1, 2, player) ||
+    checkRow(3, 4, 5, player) ||
+    checkRow(6, 7, 8, player) ||
+    checkRow(0, 3, 6, player) ||
+    checkRow(1, 4, 7, player) ||
+    checkRow(2, 5, 8, player) ||
+    checkRow(0, 4, 8, player) ||
+    checkRow(2, 4, 6, player)
+  ) result = true;
+  
+  return result;
+}
+
+function checkRow(a, b, c, player) {
+  let result = false;
+  
+  if (getSquare(a) === player && getSquare(b) === player && getSquare(c) === player) {
+    result = true;
+  }
+  
+  return result
+}
+
+function getSquare(box) {
+  return document.getElementById(box).innerText;
+}
